2fdcf1763c0ba6c0cbdfff300442dbefe6624024
Jonah <jonahh0987@gmail.com>
2024-09-29 18:03:45 -0400

    update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,7 @@ public class PlayerMovement : MonoBehaviour
         float turn = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
 
         transform.Translate(0, 0, move);
+
         transform.Rotate(0, turn, 0);
     }
 }
