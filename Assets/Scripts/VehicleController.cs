384aa25c54b1a5f6d8df43277cb9428048aa256b
chvelez19 <chvelez19@gmail.com>
2024-10-13 16:02:55 -0400

    Added Part of Nitro

--- Assets/Scripts/VehicleController.cs
+++ Assets/Scripts/VehicleController.cs
@@ -5,18 +5,38 @@ using UnityEngine;
 public class VehicleController : MonoBehaviour
 {
     public float speed;
-    public float turnSpeed;
+    public float oil;
 
     private Rigidbody rb;
+    private float[] angles = { 0, 45, 90, 135, 180, 225, 270, 315};
+    private int curAngle = 0;
+    private bool turnPressed = false;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        transform.eulerAngles = new Vector3(0, angles[curAngle], 0);
     }
 
     // Update is called once per frame
     void FixedUpdate()
+    {
+        Drive();
+        Turn();
+        Nitro();
+    }
+
+    void Nitro()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && oil > 0)
+        {
+            rb.AddRelativeForce(Vector3.forward * 100);
+            oil--;
+        }
+    }
+
+    void Drive()
     {
         if (Input.GetKey(KeyCode.UpArrow))
         {
@@ -26,17 +46,39 @@ public class VehicleController : MonoBehaviour
         {
             rb.AddRelativeForce(-Vector3.forward * speed * 10);
         }
-        Vector3 localVelocity = transform.InverseTransformDirection(rb.velocity);
-        localVelocity.x = 0;
-        rb.velocity = transform.TransformDirection(localVelocity);
-        print(rb.velocity);
-        if (Input.GetKey(KeyCode.RightArrow))
+    }
+
+    void Turn()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) && !turnPressed)
         {
-            rb.AddTorque(Vector3.up * turnSpeed);
+            if (curAngle == 7)
+            {
+                curAngle = 0;
+            }
+            else
+            {
+                curAngle++;
+            }
+            transform.eulerAngles = new Vector3(0, angles[curAngle], 0);
+            turnPressed = true;
         }
-        else if (Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.LeftArrow) && !turnPressed)
         {
-            rb.AddTorque(-Vector3.up * turnSpeed);
+            if (curAngle == 0)
+            {
+                curAngle = 7;
+            }
+            else
+            {
+                curAngle--;
+            }
+            transform.eulerAngles = new Vector3(0, angles[curAngle], 0);
+            turnPressed = true;
+        }
+        else if (!Input.GetKey(KeyCode.RightArrow)  && !Input.GetKey(KeyCode.LeftArrow) && turnPressed) 
+        {
+            turnPressed = false;
         }
     }
 }
