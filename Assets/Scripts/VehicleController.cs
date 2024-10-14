7bf35bcfa1fcc2e8c5124bede810e3c161ffd1b1
chvelez19 <chvelez19@gmail.com>
2024-10-14 17:34:19 -0400

    Changed Back to VehicleController

--- Assets/Scripts/VehicleController.cs
+++ Assets/Scripts/VehicleController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class VehicleController : MonoBehaviour
 {
     public float speed;
     public float oil;
