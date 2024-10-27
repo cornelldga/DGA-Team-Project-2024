1f3a514835ba4a6c022a7fc9a2f111f71642ecc6
chvelez19 <chvelez19@gmail.com>
2024-10-27 14:26:53 -0400

    Added Steering and Drifting

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,10 +13,12 @@ public class Player : MonoBehaviour
     public float cookingTime = 60f; // Total cooking time in seconds
 
     [SerializeField] private KeyCode nitro = KeyCode.LeftShift;
+    [SerializeField] private KeyCode drift = KeyCode.Space;
 
     private Rigidbody rb;
     private float[] angles = { 0, 45, 90, 135, 180, 225, 270, 315 };
     private int curAngle = 0;
+    private bool movingForward = false;
 
     //New added private variables 
 
@@ -37,7 +39,8 @@ public class Player : MonoBehaviour
     {
         Drive();
         Nitro();
-        //Cook();
+        Steer();
+        Drift();
     }
 
     private void Update()
@@ -55,69 +58,119 @@ public class Player : MonoBehaviour
         }
     }
 
-    // The player can press up and down arrows to drive forwards and backwards.
+    // The player can press W and S to drive forwards and backwards.
     void Drive()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.W))
         {
             rb.AddRelativeForce(Vector3.forward * speed * 10);
+            movingForward = true;
         }
-        else if (Input.GetKey(KeyCode.DownArrow))
+        else if (Input.GetKey(KeyCode.S))
         {
             rb.AddRelativeForce(-Vector3.forward * speed * 10);
+            movingForward = false;
+        }
+        else
+        {
+            movingForward = false;
         }
     }
 
-    // The player can use left or right arrow to turn to the next of 8 possible directions.
+    // The player can use A and D to turn to the next of 8 possible directions.
     void Turn()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (!Input.GetKey(drift))
         {
-            if (curAngle == 7)
+            if (Input.GetKeyDown(KeyCode.D))
             {
-                curAngle = 0;
+                if (curAngle == 7)
+                {
+                    curAngle = 0;
+                }
+                else
+                {
+                    curAngle++;
+                }
+                transform.eulerAngles = new Vector3(0, angles[curAngle], 0);
             }
-            else
+            else if (Input.GetKeyDown(KeyCode.A))
             {
-                curAngle++;
+                if (curAngle == 0)
+                {
+                    curAngle = 7;
+                }
+                else
+                {
+                    curAngle--;
+                }
+                transform.eulerAngles = new Vector3(0, angles[curAngle], 0);
             }
-            transform.eulerAngles = new Vector3(0, angles[curAngle], 0);
         }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        else if (Input.GetKey(drift))
         {
-            if (curAngle == 0)
+            if (Input.GetKeyDown(KeyCode.D))
             {
-                curAngle = 7;
+                if (curAngle == 7)
+                {
+                    curAngle = 1;
+                }
+                else if (curAngle == 6)
+                {
+                    curAngle = 0;
+                }
+                else 
+                { 
+                    curAngle += 2;
+                }
+                transform.eulerAngles = new Vector3(0, angles[curAngle], 0);
             }
-            else
+            else if (Input.GetKeyDown(KeyCode.A))
             {
-                curAngle--;
+                if (curAngle == 0)
+                {
+                    curAngle = 6;
+                }
+                else if (curAngle == 1)
+                {
+                    curAngle = 7;
+                }
+                else
+                {
+                    curAngle -= 2;
+                }
+                transform.eulerAngles = new Vector3(0, angles[curAngle], 0);
             }
-            transform.eulerAngles = new Vector3(0, angles[curAngle], 0);
         }
     }
 
-    //Cook method continuously decreases the cookingTimer and oil 
-    /*void Cook()
+    // Player can hold right and left arrows to steer left and right
+    void Steer()
     {
-        if (cookingTimer > 0)
+        if (rb.velocity.magnitude > 0.01f)
         {
-            cookingTimer -= Time.deltaTime;
-            oil -= oilConsumptionRate * Time.deltaTime;
-
-            if (oil <= 0)
+            if (Input.GetKey(KeyCode.RightArrow))
             {
-                oil = 0;
-                Debug.Log("Oil depleted!");
+                rb.AddRelativeForce(Vector3.right * 10);
             }
-
-            if (cookingTimer <= 0)
+            else if (Input.GetKey(KeyCode.LeftArrow))
             {
-                cookingTimer = 0;
-                Debug.Log("Cooking complete!");
+                rb.AddRelativeForce(Vector3.left * 10);
             }
         }
-    }*/
+    }
+
+    // Player can hold the spacebar to brake and turn while braking to drift
+    void Drift()
+    {
+        if (Input.GetKey(drift))
+        {
+            if (movingForward && rb.velocity.magnitude > 0.01f)
+            {
+                rb.AddRelativeForce(-Vector3.forward * speed * 10);
+            }
+        }
+    }
 
     /// <summary>
     /// Called every frame to check through the list of customers and decrease cooking time and oil
