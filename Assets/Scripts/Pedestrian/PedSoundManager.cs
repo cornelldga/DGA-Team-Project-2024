4e3015132c317f52fb38ff582c22f0045a38ce7a
Haoxuan Zou <porridgeorge@gmail.com>
2025-03-23 16:08:47 -0400

    Fixing ped/customer scale problem

--- Assets/Scripts/Pedestrian/PedSoundManager.cs
+++ Assets/Scripts/Pedestrian/PedSoundManager.cs
@@ -6,24 +6,11 @@ using UnityEngine;
 public class PedSoundManager : MonoBehaviour
 {
     private PedAnimManager pedAnimManager;
-    //private AudioSource tempAudioSource; // Temporary AudioSource for playing sounds //Commented out since we need to use the AudioManager
-    [SerializeField] AudioClip[] OrderFailedSounds;
 
     void Start()
     {
         pedAnimManager = GetComponent<PedAnimManager>();
-
-        // Create an AudioSource dynamically if not already attached
-        //tempAudioSource = gameObject.AddComponent<AudioSource>();
     }
-
-    //private void PlayClip(AudioClip clip)
-    //{
-    //    if (clip != null)
-    //    {
-    //        tempAudioSource.PlayOneShot(clip);
-    //    }
-    //}
 
     public void PlayOrderCompleteSound(Vector3 position)
     {
