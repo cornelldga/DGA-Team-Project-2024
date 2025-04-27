62469f3fe04f9af19ba662495f85ba428d0a4cb1
emily-penna <emilypenna99@gmail.com>
2025-04-27 17:39:28 -0400

    cop spawns after an order is taken

---
+++ Assets/Scripts/Tutorial/TutorialScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialScript : MonoBehaviour
+{
+    [SerializeField] private GameObject cop;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (GameManager.Instance.GetCustomers().Count > 0)
+        {
+            cop.SetActive(true);
+        }
+    }
+}
