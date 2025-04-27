c37f977e6c11bb89ef7fe27ca8ad279bfa65063d
Jordan-Rudolph <chubbyseventeen17@gmail.com>
2025-04-27 15:56:44 -0400

    Add cooldown to pedestrian sfx, add building collision sound

--- Assets/Scripts/Pedestrian/PedSoundManager.cs
+++ Assets/Scripts/Pedestrian/PedSoundManager.cs
@@ -6,34 +6,48 @@ using UnityEngine;
 public class PedSoundManager : MonoBehaviour
 {
     private PedAnimManager pedAnimManager;
+    private const float soundCooldown = 5f;
+    private float prevSoundTime = 0f;
 
     void Start()
     {
         pedAnimManager = GetComponent<PedAnimManager>();
     }
 
+
     public void PlayOrderCompleteSound(Vector3 position)
     {
-        //Debug.Log(pedAnimManager.GetRandomOrderCompleteSound());
-        int randomIndex = Random.Range(1, 3);
-        string sfx_id = pedAnimManager.GetRandomOrderCompleteSound() + randomIndex;
-        AudioManager.Instance.PlaySoundAtPoint(sfx_id, position);
+        if (Time.time - prevSoundTime >= soundCooldown)
+        {
+            //Debug.Log(pedAnimManager.GetRandomOrderCompleteSound());
+            int randomIndex = Random.Range(1, 3);
+            string sfx_id = pedAnimManager.GetRandomOrderCompleteSound() + randomIndex;
+            AudioManager.Instance.PlaySoundAtPoint(sfx_id, position);
+            prevSoundTime = Time.time;
+        }
     }
 
     public void PlayHurtSound(Vector3 position)
     {
-        //Debug.Log(pedAnimManager.GetRandomHurtSound());
-        int randomIndex = Random.Range(1, 3);
-        string sfx_id = pedAnimManager.GetRandomHurtSound() + randomIndex;
-        AudioManager.Instance.PlaySoundAtPoint(sfx_id, position);
+        if (Time.time - prevSoundTime >= soundCooldown)
+        {
+            //Debug.Log(pedAnimManager.GetRandomHurtSound());
+            int randomIndex = Random.Range(1, 3);
+            string sfx_id = pedAnimManager.GetRandomHurtSound() + randomIndex;
+            AudioManager.Instance.PlaySoundAtPoint(sfx_id, position);
+            prevSoundTime = Time.time;
+        }
     }
 
     public void PlayTakeOrderSound(Vector3 position)
     {
-        //Debug.Log(pedAnimManager.GetRandomTakeOrderSound());
-        int randomIndex = Random.Range(1, 3);
-        string sfx_id = pedAnimManager.GetRandomTakeOrderSound() + randomIndex;
-        AudioManager.Instance.PlaySoundAtPoint(sfx_id, position);
+        if (Time.time - prevSoundTime >= soundCooldown) { 
+            //Debug.Log(pedAnimManager.GetRandomTakeOrderSound());
+            int randomIndex = Random.Range(1, 3);
+            string sfx_id = pedAnimManager.GetRandomTakeOrderSound() + randomIndex;
+            AudioManager.Instance.PlaySoundAtPoint(sfx_id, position);
+            prevSoundTime = Time.time;
+        }
     }
 
     public void PlayOrderFailedSound(Vector3 position)
