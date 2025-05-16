cc8212f86c0582d57edb41757a60e2e0412219b6
emily-penna <emilypenna99@gmail.com>
2025-05-16 16:15:52 -0400

    added new cooking minigame to tutorial

--- Assets/Scripts/Tutorial/TutorialScript.cs
+++ Assets/Scripts/Tutorial/TutorialScript.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 
 public class TutorialScript : MonoBehaviour
 {
-    [SerializeField] private GameObject cop;
     [SerializeField] GameObject tooltip;
     [SerializeField] TMP_Text tooltipMessage;
     [SerializeField] Button OKButton;
@@ -19,12 +18,24 @@ public class TutorialScript : MonoBehaviour
     // the number of seconds a message remains on screen before disappearing.
     //[SerializeField] private float messageDuration = 10;
     //private float messageTimer = 0;
-    private bool activeCop = false;
+    private bool playedCookingTutorial1 = false;
+    private bool playedCookingTutorial2 = false;
+    private bool playedCookingTutorial3 = false;
+    private bool playedCookingTutorial4 = false;
 
+    private bool activeCop = false;
+    [SerializeField] private GameObject cop;
+    // the customer that is visited first.
+    [SerializeField] private Customer customer1;
+    // spawn in this customer after customer 1 is served. 
+    [SerializeField] private GameObject customer2;
 
     private void Start()
     {
         objectiveLabel.SetActive(false);
+        cop.SetActive(false);
+        customer2.SetActive(false);
+
         GameManager.Instance.FreezeGame();
 
         GameManager.Instance.getPlayer().TurnRight();
@@ -34,26 +45,51 @@ public class TutorialScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // only show tooltip message for a set amount of time
-        //if (messageTimer > 0)
-        //{
-        //    Debug.Log(messageTimer);
-        //    messageTimer -= Time.deltaTime;
-        //    if (messageTimer <= 0)
-        //    {
-        //        tooltip.SetActive(false);
-        //    }
-        //}
-
-
-        // spawn cop after first order is taken. 
-        if (!activeCop && GameManager.Instance.GetCustomers().Count > 0)
-        {
-            cop.SetActive(true);
-            activeCop = true;
-            ShowMessage("They found us! We can't vend food in this city, but that won't stop us. Avoid the cops until the order is ready!");
-        }
+        cookingScript();
     }
+
+    void cookingScript()
+    {
+        
+        if (!playedCookingTutorial1 && GameManager.Instance.GetCustomers().Count > 0)
+        {
+            playedCookingTutorial1 = true;
+            ShowMessage("Now cook their order! Left click the food on the stove until its perfect!");
+            setObjectiveMessage("Left click food to cook");
+        }
+
+        if (!playedCookingTutorial2 && customer1.IsFoodReady())
+        {
+            playedCookingTutorial2 = true;
+            ShowMessage("Looks great! The order is complete. Press 'E' again to fulfill the order.");
+            setObjectiveMessage("Press 'E' to serve customer");
+        }
+
+        if (!playedCookingTutorial3 && customer1.IsOrderCompleted())
+        {
+            playedCookingTutorial3 = true;
+            customer2.SetActive(true);
+
+            ShowMessage("There are more hungry bugs. Lets find our next customer!");
+            setObjectiveMessage("Find and serve another costumer");
+            
+        }
+
+        // spawn cop after second order is taken.
+        if (playedCookingTutorial3 && !playedCookingTutorial4 && GameManager.Instance.GetCustomers().Count > 0)
+        {
+            playedCookingTutorial4 = true;
+            cop.SetActive(true);
+
+            ShowMessage("The cops! They found us! Keep cooking but speed away with 'SHIFT'!");
+            setObjectiveMessage("Finish order while avoiding cops");
+
+        }
+
+
+
+    }
+
 
     // Method called when OK button is pressed
     public void OKButtonPressed()
